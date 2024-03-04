5212a23a3a28590aef84fc861008e21aa8ab5495
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-03-04 21:33:23 +0900

    study

--- JavaScript/dom2.js
+++ JavaScript/dom2.js
@@ -1,10 +1,7 @@
 window.onload = function() {
-  // h1태그와 text node를 생성
   var h1 = document.createElement('h1');
   var txtNode = document.createTextNode('Hello');
 
   h1.appendChild(txtNode);
-  document.body.appendChild(h1);  // h1태그를 body태그에 추가
-
-
+  document.body.appendChild(h1);
 }
