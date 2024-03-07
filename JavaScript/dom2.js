9e8b0b5ef2bca541574817be1dc53a9f50332827
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-03-07 09:55:30 +0900

    study

--- JavaScript/dom2.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  var h1 = document.createElement('h1');
-  var txtNode = document.createTextNode('Hello');
-
-  h1.appendChild(txtNode);
-  document.body.appendChild(h1);
-}
